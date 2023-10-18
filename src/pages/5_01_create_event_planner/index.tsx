81bba248ec5bc80e7acc1da213ffe1c7105101c5
diduladishan <diduladishan14@gmail.com>
2023-10-18 17:24:17 +0530

    first commit

---
+++ src/pages/5_01_create_event_planner/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@material-tailwind/react";
+import EVENTPLANNER_1 from "../../assets/event planner/Add User-rafiki 1.png";
+import EVENTPLANNER_2 from "../../assets/event planner/create your account.png";
+import EVENTPLANNER_3 from "../../assets/event planner/post an event.png";
+import EVENTPLANNER_4 from "../../assets/event planner/choose the best proposal.png";
+import Done_Icon from "../../assets/AV Providers/done-icon.png";
+
+function index() {
+  return <div></div>;
+}
+
+export default index;
