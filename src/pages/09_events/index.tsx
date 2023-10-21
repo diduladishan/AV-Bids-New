f1c82574dd4aab8ff91f1ade0ec57a6ff9a7f889
diduladishan <diduladishan14@gmail.com>
2023-10-21 18:32:01 +0530

    event listings updated

--- src/pages/09_events/index.tsx
+++ src/pages/09_events/index.tsx
@@ -18,11 +18,10 @@ import EVENTS_02 from "../../assets/09_events/location.png";
 export function index() {
   return (
     <div>
-      {" "}
       <div>
-        <h2 className="text-center text-primary">Event Listings</h2>
+        <h2 className="text-center text-primary mb-16">Event Listings</h2>
       </div>
-      <div className="flex w-full ">
+      <div className="flex justify-center w-full gap-8 ">
         <Card className="h-[calc(120vh-2rem)] w-full max-w-[20rem] p-4  bg-[#F3F1FB]">
           <div className="mb-2 p-4">
             <div>
@@ -130,60 +129,79 @@ export function index() {
                 variant="filled"
                 color="indigo"
                 size="sm"
-                className="rounded-md w-36 py-4 mt-4 px-8 bg-primary font-poppins"
+                className="rounded-md  py-4 mt-4 px-8 bg-primary font-poppins"
               >
-                <span className="text-white">Get Started</span>
+                <span className="text-white">Apply Filters</span>
               </Button>
             </div>
           </div>
         </Card>
 
-        <div className="flex justify-between bg-[#889867] w-full">
-          <div>
-            <img
-              src={EVENTS_01}
-              alt="aad"
-              className="object-scale-down w-[73px]"
-            />
+        <div>
+          <div className="flex justify-between items-center mb-6">
+            <p>500 events Found</p>
+            <div className="w-[200px]">
+              <Select label="Sort: Ending Soonest ">
+                <Option>Ending Soonest</Option>
+                <Option>Budget Lowest</Option>
+                <Option>Budget Highest</Option>
+                <Option>Audience Size Lowest</Option>
+                <Option>Audience Size Highest</Option>
+              </Select>
+            </div>
           </div>
-
-          <div>
-            <h2 className="text-[20px]">Virtual Sales Training</h2>
-
-            <div className="flex">
-              <p className="text-[18px]">Event Date: 10/10/2023 - 10/15/2023</p>
-              <p className="text-[18px]">$70,000 - $150,000</p>
+          <div className="flex items-center justify-center bg-[#fff] drop-shadow gap-8 p-8 h-40 rounded-lg w-max">
+            <div>
+              <img
+                src={EVENTS_01}
+                alt="aad"
+                className="object-scale-down w-[73px]"
+              />
             </div>
 
-            <div className="flex">
-              <img
-                src={EVENTS_02}
-                alt="aad"
-                className="object-scale-down w-[20px]"
-              />
-              <p className="text-[16px] text-[#9381FF]">Phoenix, Arizona</p>
-              <p className="text-[16px]">Corporate, General Meeting</p>
+            <div>
+              <h2 className="text-[20px]">General Session & 4 Breakouts</h2>
+
+              <div className="flex gap-36">
+                <p className="text-[18px]">
+                  Event Date: 10/10/2023 - 10/15/2023
+                </p>
+                <p className="text-[18px]">$70,000 - $150,000</p>
+              </div>
+
+              <div className="flex items-center gap-16 mt-4">
+                <div className="flex gap-8 items-center">
+                  <img
+                    src={EVENTS_02}
+                    alt="aad"
+                    className="object-scale-down w-[20px]"
+                  />
+                  <p className="text-[16px] text-[#9381FF]">Phoenix, Arizona</p>
+                </div>
+
+                <p className="text-[16px]">Corporate, General Meeting</p>
+                <Button
+                  variant="filled"
+                  color="green"
+                  size="sm"
+                  className="rounded-full w-30 py-2 px-3 bg-[#B5F9C4] font-poppins"
+                >
+                  <h2 className="text-[#178751] text-[12px]">Virtual</h2>
+                </Button>
+              </div>
+            </div>
+
+            <div className="ml-24">
               <Button
                 variant="filled"
-                color="green"
+                color="indigo"
                 size="sm"
-                className="rounded-full w-30 py-2 px-3 bg-[#B5F9C4] font-poppins"
+                className="rounded-md w-36 py-4 mt-4 px-8 bg-primary font-poppins"
               >
-                <h2 className="text-[#178751] text-[15px]">Virtual</h2>
+                <span className="text-white">Apply Now</span>
               </Button>
+              <p className="text-[16px] mt-4">23 days left to apply</p>
             </div>
-          </div>
-
-          <div>
-            <Button
-              variant="filled"
-              color="indigo"
-              size="sm"
-              className="rounded-md w-36 py-4 mt-4 px-8 bg-primary font-poppins"
-            >
-              <span className="text-white">Apply Now</span>
-            </Button>
-            <p className="text-[16px]">23 days left to apply</p>
           </div>
         </div>
       </div>
